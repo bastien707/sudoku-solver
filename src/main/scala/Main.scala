5471e855713ecc67d6fdce0d881fed1ddc1b704e
joel <joelgabbai@gmail.com>
2023-06-27 13:11:29 +0200

    create type sudoku grid and add prettyprint function

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,6 +4,31 @@ import zio._
 
 object Main extends ZIOAppDefault {
 
+  type SudokuGrid = Vector[Vector[Option[Int]]]
+  val initialGrid: SudokuGrid = Vector( 
+    Vector(Some(5), Some(3), None, None, Some(7), None, None, None, None), 
+    Vector(Some(6), None, None, Some(1), Some(9), Some(5), None, None, None), 
+    Vector(None, Some(9), Some(8), None, None, None, None, Some(6), None), 
+    Vector(Some(8), None, None, None, Some(6), None, None, None, Some(3)), 
+    Vector(Some(4), None, None, Some(8), None, Some(3), None, None, Some(1)), 
+    Vector(Some(7), None, None, None, Some(2), None, None, None, Some(6)), 
+    Vector(None, Some(6), None, None, None, None, Some(2), Some(8), None), 
+    Vector(None, None, None, Some(4), Some(1), Some(9), None, None, Some(5)), 
+    Vector(None, None, None, None, Some(8), None, None, Some(7), Some(9))
+  )
+
+  def prettyPrint(sudoku: SudokuGrid): String = {
+    sudoku.grouped(3).map { bigGroup =>
+      bigGroup.map { row =>
+        row.grouped(3).map { smallGroup =>
+          smallGroup.map(cell => 
+            cell.getOrElse("-")
+          ).mkString(" ", " ", " ")
+        }.mkString("|", "|", "|")
+      }.mkString("\n")
+    }.mkString("+-------+-------+-------+\n", "\n+-------+-------+-------+\n", "\n+-------+-------+-------+")
+  }
+
   def run: ZIO[Any, Throwable, Unit] =
     for {
       _ <- Console.print("Enter the path to the JSON file containing the Sudoku problem:")
